8147f07321058fa90f00f4f92ec5f37cf90eb8cc
Cesar Valdez <cesarv01@gmail.com>
2014-05-29 10:24:53 +0200

    first commit

---
+++ lazyload/main.js
@@ -0,0 +1,11 @@
+(function () {
+
+lazy.loadjs(["//cdnjs.cloudflare.com/ajax/libs/jquery/1.11.0/jquery.js", 
+			 "//cdnjs.cloudflare.com/ajax/libs/jqueryui/1.10.4/jquery-ui.min.js",
+			 "//cdnjs.cloudflare.com/ajax/libs/json2/20130526/json2.min.js"],
+			  function(){
+
+			 	$('.titulo').text("Hello world");
+			 });
+
+}());
